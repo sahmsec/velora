b569432d7b1445bc2e11c7ebe296ce593a148b74
Sakib Ahmed <sakibahmedsadhin@gmail.com>
2026-01-27 11:07:15 +0000

    Polish UI transitions

---
+++ components/SmoothScroll.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { ReactLenis } from "lenis/react";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+
+export default function SmoothScroll({ children }) {
+  const lenisRef = useRef();
+
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+    gsap.ticker.add(update);
+    return () => {
+      gsap.ticker.remove(update);
+    };
+  }, []);
+
+  return (
+    <ReactLenis root ref={lenisRef} autoRaf={false} options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+      {children}
+    </ReactLenis>
+  );
+}
